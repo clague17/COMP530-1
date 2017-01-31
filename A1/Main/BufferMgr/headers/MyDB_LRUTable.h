b2a1bcc0f301eb50e3dfa171f9bd29de7879e0da
unknown <crespo5609@gmail.com>
2017-01-31 17:24:10 -0600

    include <memory> to page and LRU table

--- A1/Main/BufferMgr/headers/MyDB_LRUTable.h
+++ A1/Main/BufferMgr/headers/MyDB_LRUTable.h
@@ -7,6 +7,7 @@
 
 #include <list>
 #include <unordered_map>
+#include <memory>
 #include "MyDB_Page.h"
 
 using namespace std;
@@ -20,34 +21,34 @@ typedef list<id_and_page_pair> :: iterator list_it;
 using namespace std;
 
 class MyDB_LRUTable {
-    
+
 public:
-    
+
     // get the evicted candidate, return a smart pointer to a page that is going to be evicted.
     // Called by buffer manager when there is no free space for pages
     MyDB_PagePtr checkLRU();
-    
+
     // update LRU table
     // if the id is in the table, shift it to the last position of access history
     // if the id is not in the table, append it to the last position of access history and lookup table
     void updateLRU(pageID updateMeID, MyDB_PagePtr updateMePtr);
-    
+
     // evict a specified item
     // return the address of page frame
     // Called by buffer manager to evict a anonymous page
     char* evictItem(pageID evictMeID);
-    
+
     void clearLRU();
-    
+
     MyDB_LRUTable();
-    
+
     ~MyDB_LRUTable();
-    
+
 private:
-    
+
     // A list of pairs of id and page pointer ordered by accessing time
     list<id_and_page_pair> _accessHistory;
-    
+
     // A look up table to find out a iterator in the access history list
     // key: pageID, value: iterator
     unordered_map<pageID, list_it> _lookUpTable;
--- A1/Main/BufferMgr/headers/MyDB_Page.h
+++ A1/Main/BufferMgr/headers/MyDB_Page.h
@@ -1,6 +1,7 @@
 #ifndef PAGE_H
 #define PAGE_H
 #include <string>
+#include <memory>
 //#include "MyDB_BufferManager.h"
 
 using namespace std;
@@ -14,105 +15,105 @@ typedef shared_ptr<MyDB_Table> MyDB_TablePtr;
 typedef string fileLoc;
 
 class MyDB_Page : public enable_shared_from_this<MyDB_Page>  {
-    
+
 public:
     // Mark the dirty bit
     void markDirty();
-    
+
     // Unmark the dirty bit
     void unmarkDirty();
-    
+
     // Mark the hit bit
     void markHit();
-    
+
     // Unmark the hit bit
     void unmarkHit();
-    
+
     // Mark the buffer bit
     void markBuffer();
-    
+
     // Unmark the buffer bit
     void unmarkBuffer();
-    
+
     // Mark the pin bit
     void markPin();
-    
+
     // Unmark the pin bit
     void unmarkPin();
-    
+
     // Return true if the page is dirty, otherwise return false
     bool isDirty();
-    
+
     // Return true if the page is buffered in RAM, otherwise return false
     bool isBuffered();
-    
+
     // Return true if the page is pinned, otherwise return false
     bool isPinned();
-    
+
     bool isAnonymous();
-    
+
     // Increase the reference counter by one
     void incRefCounter();
-    
+
     // Decrease the reference counter by one
     void decRefCounter();
-    
+
     bool hasNoCounter();
-    
+
     // return PageID
     string getPageID();
-    
+
     // return the page frame
     char* getPageFrame();
-    
+
     // return the address of the actual data
     pair<fileLoc, int> getAddress();
-        
+
     // Buffer the data from file to RAM
     void bufferMyself();
-    
+
     // Evict the page
     void evictMyself();
-    
+
     void updateMyselfinLRU();
-    
+
     void writeBack();
-    
+
     MyDB_Page(MyDB_BufferManager* const& bufferManager, pair<fileLoc, int> const& addressinStorage, string const& pageID, bool isAnonymous);
-    
+
     ~MyDB_Page();
-    
+
 private:
     // the size of page
     size_t _pageSize;
-    
+
     // the counter of handles to the page object
     int _refCounter;
-    
+
     // a bit to indicate whether a page is dirty
     bool _dirtyBit;
-    
+
     // a bit to indicate whether a page is hit
     bool _hitBit;
-    
+
     // a bit to indicate whether a page is buffered in RAM
     bool _bufferBit;
-    
+
     // a bit to indicate whether a page is pinned
     bool _pinBit;
-    
+
     // a bit to indicate whether a page is anonymous
     bool _anonBit;
-    
+
     // a unique page ID
     string _pageID;
-    
+
     // the address in storage
     pair<fileLoc, int> _addressinStorage;
-    
+
     // the address of the page frame where the page is buffered in RAM
      char* _pageFrame;
-    
+
     // the buffer manager
     MyDB_BufferManager* _bufferManager;
 };
